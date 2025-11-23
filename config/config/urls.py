9c0ba739a749f1e21b36c5dfa96b1a86a0498e50
mcpalta <mauriciocatalanmedina@gmail.com>
2025-11-23 16:44:19 -0300

    first commit

---
+++ config/config/urls.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from usuarios.views import logout_view
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="usuarios/login.html"),
+        name="login"
+    ),
+
+    path("logout/", logout_view, name="logout"),
+
+    path("usuarios/", include("usuarios.urls")),
+    path("productos/", include("productos.urls")),
+    path("", include("core.urls")),
+        path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="usuarios/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="usuarios/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="usuarios/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="usuarios/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
