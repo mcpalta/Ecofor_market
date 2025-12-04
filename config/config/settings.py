8c92f6f45056f4873aeeecc9abe17fd23f4159fd
Ubuntu <ubuntu@ip-172-31-46-150.us-east-2.compute.internal>
2025-12-04 04:14:41 +0000

    prueba 4 sv

--- config/config/settings.py
+++ config/config/settings.py
@@ -141,8 +141,6 @@ DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "no-reply@ecofor-marke
 # -----------------------
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 if not DEBUG:
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
     SECURE_SSL_REDIRECT = False
     SECURE_HSTS_SECONDS = 60
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
